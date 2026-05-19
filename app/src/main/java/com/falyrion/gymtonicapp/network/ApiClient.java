ca8421566383ba37966f5b03e0dc5322d8a5e551
diorenegregavalic-hub <diorenegregabalic@gmail.com>
2026-05-19 12:04:20 +0800

    Update ApiClient Crud

--- app/src/main/java/com/falyrion/gymtonicapp/network/ApiClient.java
+++ app/src/main/java/com/falyrion/gymtonicapp/network/ApiClient.java
@@ -8,8 +8,10 @@ import com.falyrion.gymtonicapp.models.WorkoutPlan;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiClient {
@@ -22,4 +24,13 @@ public interface ApiClient {
 
     @GET("api/workouts/plans/{user_id}")
     Call<List<WorkoutPlan>> getWorkoutPlans(@Path("user_id") int userId);
+
+    @POST("api/workouts/plans")
+    Call<AuthResponse> createWorkoutPlan(@Body WorkoutPlan plan); // Pwede AuthResponse o GenericResponse ang gamiton basta naay success ug message
+
+    @PUT("api/workouts/plans/{id}")
+    Call<AuthResponse> updateWorkoutPlan(@Path("id") int id, @Body WorkoutPlan plan);
+
+    @DELETE("api/workouts/plans/{id}")
+    Call<AuthResponse> deleteWorkoutPlan(@Path("id") int id);
 }
